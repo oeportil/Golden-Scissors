22e6958ebe99131c647b94b1ec0dde9a23f6ff94
xaldakr <xavieravilaposada@gmail.com>
2024-04-23 13:52:58 -0600

    Funcion de imagenes, un dolor
    Funciona

--- src/pages/api/images/[id].js
+++ src/pages/api/images/[id].js
@@ -9,49 +9,57 @@ import path from "path";
 
 //En el body se tiene que enviar una imagen y un int, si el int es 0, tomará el ultimo valor, sino, tomará ese int
 export default async function handler(req, res) {
-  if (req.method === "GET") {
-    //Obtener la ruta del archivo
-    const { tipo, identity } = req.body;
-    let tipoarchivo;
-    let extent = null;
-    switch (tipo) {
-      case "1":
-        tipoarchivo = "usuarios";
-        break;
-      case "2":
-        tipoarchivo = "blog";
-        break;
-      case "3":
-        tipoarchivo = "servicios";
-        break;
-      case "4":
-        tipoarchivo = "empleados";
-        break;
-    }
-    console.log(tipoarchivo);
-    let ruta = path.join(process.cwd(), "public", tipoarchivo);
-    const archivos = readdirSync(ruta);
-    console.log(ruta);
-    for (const archivo of archivos) {
-      const rutaArchivo = path.join(ruta, archivo);
+  if (req.method === "PUT") {
+    try {
+      //Obtener la ruta del archivo
+      let { tipo, identity } = req.body;
+      console.log(req.body);
+      tipo = tipo.toString();
+      identity = identity.toString();
 
-      if (statSync(rutaArchivo).isFile()) {
-        //const probarextent = path.name(rutaArchivo);
-        if (
-          archivo == identity + ".jpg" ||
-          archivo == identity + ".png" ||
-          archivo == identity + ".gif" ||
-          archivo == identity + ".jpeg"
-        ) {
-          extent = archivo;
+      let tipoarchivo;
+      let extent = null;
+      switch (tipo) {
+        case "1":
+          tipoarchivo = "usuarios";
+          break;
+        case "2":
+          tipoarchivo = "blog";
+          break;
+        case "3":
+          tipoarchivo = "servicios";
+          break;
+        case "4":
+          tipoarchivo = "empleados";
+          break;
+      }
+      console.log(tipoarchivo);
+      let ruta = path.join(process.cwd(), "public", tipoarchivo);
+      const archivos = readdirSync(ruta);
+      console.log(ruta);
+      for (const archivo of archivos) {
+        const rutaArchivo = path.join(ruta, archivo);
+
+        if (statSync(rutaArchivo).isFile()) {
+          //const probarextent = path.name(rutaArchivo);
+          if (
+            archivo == identity + ".jpg" ||
+            archivo == identity + ".png" ||
+            archivo == identity + ".gif" ||
+            archivo == identity + ".jpeg"
+          ) {
+            extent = archivo;
+          }
         }
       }
-    }
-    if (extent != null) {
-      ruta = path.join(process.cwd(), "public", tipoarchivo, extent);
-      res.json(ruta);
-    } else {
-      res.json(path.join(process.cwd(), "public", tipoarchivo, "def.jpg"));
+      if (extent != null) {
+        ruta = extent;
+        res.json(ruta);
+      } else {
+        res.json("def.jpg");
+      }
+    } catch {
+      res.json(req.body);
     }
   } else if (req.method === "POST") {
     //Subir archivos
