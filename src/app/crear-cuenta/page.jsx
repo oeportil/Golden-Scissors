50984a385d073660c8054d8a2a9dff83b1368010
Oscar Portillo <Oscaritoportilllo@outlook.com>
2024-04-23 10:52:29 -0600

    Rutas protegidas finalizado y agregar campo de imagen en crear cuenta

--- src/app/crear-cuenta/page.jsx
+++ src/app/crear-cuenta/page.jsx
@@ -202,7 +202,11 @@ const CrearCuenta = () => {
             >
               Repetir Contraseña
             </label>
-          </div>
+          </div>          
+            <label className="block mb-2 text-sm font-medium text-gray-500 text-start w-full dark:text-white" htmlFor="file_input">Foto de Perfil</label>
+            <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file"/>
+            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
+
           <input
             type="submit"
             value={"Crear una cuenta"}
--- src/middleware.js
+++ src/middleware.js
@@ -2,13 +2,39 @@
 import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 
-const protectedRoutes = ['/dashboard']
-const publicRoutes = ['/login', '/crear-cuenta', '/', '/nosotros', '/servicios', '/blog']
 
-export default async function protectR(req) {
-    const path = req.nextUrl.pathname
-    const isProtectedRoute = protectedRoutes.includes(path)
-    const isPublicRoute = publicRoutes.includes(path)
-    const token = cookies().get('token')?.value
+export function middleware(req) {
+    const token = req.cookies.get("token")?.value
 
+    if(req.nextUrl.pathname.startsWith('/dashboard') && !token){
+        const response = NextResponse.redirect(new URL('/login', req.url))
+        return response
+    }
+
+    if(token && req.nextUrl.pathname.startsWith('/login') ){
+        const data = JSON.parse(token)   
+        if(data.admin){
+            const response = NextResponse.redirect(new URL('/dashboard/admin', req.url))
+            return response
+        } else {
+            const response = NextResponse.redirect(new URL('/dashboard/user', req.url))
+            return response
+        }
+    }
+
+    if(token && req.nextUrl.pathname === '/dashboard/admin'){
+        const data = JSON.parse(token)   
+        if(!data.admin){
+            const response = NextResponse.redirect(new URL('/dashboard/user', req.url))
+            return response
+        }
+    }
+
+    if(token && req.nextUrl.pathname === '/dashboard/user'){
+        const data = JSON.parse(token)   
+        if(data.admin){
+            const response = NextResponse.redirect(new URL('/dashboard/admin', req.url))
+            return response
+        }
+    }
 }   
