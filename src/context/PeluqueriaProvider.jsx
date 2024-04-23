1eeb10d0313dfbe233737572f0c95ebe802367fb
leBryanXD <bryan.vasquez@catolica.edu.sv>
2024-04-22 22:20:11 -0600

    servicios casi terminado

--- src/context/PeluqueriaProvider.jsx
+++ src/context/PeluqueriaProvider.jsx
@@ -1,11 +1,29 @@
 "use client";
-import { createContext } from "react";
+import { getServices } from "@/controllers/ServiciosController";
+import { createContext, useEffect, useState } from "react";
 
 const peluqueriaContext = createContext();
 
 const PeluqueriaProvider = ({ children }) => {
+  const [services, setServices] = useState([]);
+  useEffect(
+    () => async () => {
+      const fetchData = async () => {
+        try {
+          const data = await getServices();
+          setServices(data);
+        } catch (error) {
+          console.error("Error fetching services data:", error);
+        }
+      };
+      if (services.length === 0) {
+        fetchData();
+      }
+    },
+    [services]
+  );
   return (
-    <peluqueriaContext.Provider value={{}}>
+    <peluqueriaContext.Provider value={{ services }}>
       {children}
     </peluqueriaContext.Provider>
   );
