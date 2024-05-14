ea22966316552bab4d6334af4c5a8ff4ec886e1e
Oscar Portillo <Oscaritoportilllo@outlook.com>
2024-05-13 22:02:20 -0600

    Modal editar y crear

--- src/app/dashboard/admin/empleados/page.jsx
+++ src/app/dashboard/admin/empleados/page.jsx
@@ -1,16 +1,50 @@
 "use client"
-import { getEmpleados } from "@/controllers/EmpleadosController";
+import { getEmpleadoById, getEmpleados } from "@/controllers/EmpleadosController";
 import { useEffect, useState } from "react"
 import { formatDate } from "@/utils/helpers";
-import Link from "next/link";
+import styled from "styled-components";
+import Modal, { ModalProvider, BaseModalBackground } from "styled-react-modal";
+import "@/styles/empleados.css";
+
+const StyledModal = Modal.styled`
+  width: 95%;
+  background-color: white;
+  opacity: ${(props) => props.opacity};
+  transition : all 0.3s ease-in-out;
+  `;
+
 
 
 
 const Page = () => {
     const [empleado, setEmpleado] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
+    const [empleadoActual, setEmpleadoActual] = useState({})
    
+    const [isOpen, setIsOpen] = useState(false);
+  const [opacity, setOpacity] = useState(0);
 
+    function toggleModal(e) {
+        setOpacity(0);
+        setIsOpen(!isOpen);
+    }
+
+    function afterOpen() {
+        setTimeout(() => {
+        setOpacity(1);
+        }, 100);
+    }
+
+    function beforeClose() {
+        return new Promise((resolve) => {
+        setOpacity(0);
+        setTimeout(resolve, 300);
+        });
+  }
+    const FadingBackground = styled(BaseModalBackground)`
+    opacity: ${(props) => props.opacity};
+    transition: all 0.3s ease-in-out;
+    `;
 
     useEffect(() => {
       const datos = async()=> {
@@ -19,6 +53,7 @@ const Page = () => {
       }
       datos()    
     }, [])
+    
     let empleados = []
     empleado.map(emp => {
         empleados.push({
@@ -39,8 +74,6 @@ const Page = () => {
   }
   const numbers = [...Array(npage + 1).keys()].slice(1)
 
-    console.log(empleados)
-
     const nextPage = () =>{
         if(currentPage !== lastIndex){
             setCurrentPage(currentPage + 1)
@@ -55,7 +88,15 @@ const Page = () => {
         setCurrentPage(id)
     }
     const Despedir = id =>{
-
+        
+    }
+    const handleEdit = id => {
+        const empleact = async() =>{
+            const data = await getEmpleadoById(id)
+            await setEmpleadoActual(data)
+        }
+        empleact()
+        toggleModal()
     }
 
   return (
@@ -98,7 +139,7 @@ const Page = () => {
                     $ {emple.salario}
                     </td>
                     <td className="px-6 py-4 flex gap-2">
-                        <Link className="bg-black text-white py-2 px-4 rounded-full hover:bg-slate-700" href={`#`}>Editar</Link>
+                        <button onClick={()=>handleEdit(emple.id)} className="bg-black text-white py-2 px-4 rounded-full hover:bg-slate-700" href={`#`}>Editar</button>
                         <button className="bg-gold px-3 text-white rounded-full hover:bg-yellow-400" onClick={() => Despedir(emple.id)}>Despedir</button>
                     </td>
                 </tr>
@@ -132,12 +173,186 @@ const Page = () => {
             </nav>
         </div>
         <div className="flex justify-center">
-            <Link href={""} className="bg-black text-white py-2 px-6 rounded-full hover:bg-slate-700">Agregar Empleado</Link>
+            <button onClick={toggleModal}  className="bg-black text-white py-2 px-6 rounded-full hover:bg-slate-700">Agregar Empleado</button>
+        </div> 
 
-            
-        </div>        
+
+       
+
+            <ModalProvider backgroundComponent={FadingBackground}>
+                <StyledModal
+                    isOpen={isOpen}
+                    afterOpen={afterOpen}
+                    beforeClose={beforeClose}
+                    onBackgroundClick={toggleModal}
+                    onEscapeKeydown={toggleModal}
+                    opacity={opacity}
+                    backgroundProps={{ opacity }}
+                >
+                    <h2 className="text-center text-xl brown font-semibold mt-2">{Object.keys(empleadoActual).length == 0 ? "Crear Empleado" : "Editar Empleado"}</h2>
+                    <div className="p-2">
+                        <form className="max-w-md mx-auto">
+                            <div className="">
+                                    <div className="grid md:grid-cols-2 md:gap-6">
+                                        <div className="relative z-0 w-full mb-5 group">
+                                            <input
+                                            type="text"
+                                            name="text"
+                                            
+                                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                            placeholder=" "
+                                            required
+                                            />
+                                            <label
+                                            htmlFor="floating_nombre"
+                                            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                            >
+                                            Nombre
+                                            </label>
+                                        </div>
+                                        <div className="relative z-0 w-full mb-5 group">
+                                            <input
+                                            type="text"
+                                            name="text"
+                                            
+                                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                            placeholder=" "
+                                            required
+                                            />
+                                            <label
+                                            htmlFor="floating_apellido"
+                                            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                            >
+                                            Apellido
+                                            </label>
+                                        </div>
+                                    </div>                           
+                                    <div className="relative z-0 w-full mb-5 group">
+                                        <input
+                                        type="email"
+                                        name="email"
+                                    
+                                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                        placeholder=" "
+                                        required
+                                        />
+                                        <label
+                                        htmlFor="floating_email"
+                                        className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                        >
+                                        Email
+                                        </label>
+                                    </div>
+                                    <div className="grid md:grid-cols-2 md:gap-6">
+                                        <div className="relative z-0 w-full mb-5 group">
+                                        <input
+                                            type="tel"
+                                            name="tel"
+                                        
+                                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                            placeholder=" "
+                                            required
+                                        />
+                                        <label
+                                            htmlFor="floating_tel"
+                                            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                        >
+                                            Telefono
+                                        </label>
+                                        </div>
+                                        <div className="relative z-0 w-full mb-5 group">
+                                            <select name="" id="genero"
+                                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                            >
+                                                <option value={true}>Masculino</option>
+                                                <option value={false}>Femenino</option>
+                                            </select>
+                                            <label
+                                                htmlFor="genero"
+                                                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                            >
+                                                Genero
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div className="grid md:grid-cols-2 md:gap-6">
+                                    <div className="relative z-0 w-full mb-5 group">
+                                            <select name="" id=""
+                                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                            >
+
+                                            </select>
+                                            <label
+                                                htmlFor="floating_last_name"
+                                                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                            >
+                                                Horarios
+                                            </label>
+                                        </div>
+                                        <div className="relative z-0 w-full mb-5 group">
+                                        <input
+                                            type="number"
+                                            name="salario"
+                                        
+                                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                            placeholder=" "
+                                            required
+                                        />
+                                        <label
+                                            htmlFor="floating_salario"
+                                            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                        >
+                                        Salario
+                                        </label>
+                                        </div>
+                                    </div>
+                                    <div className="relative z-0 w-full mb-5 group">
+                                        <textarea name="" id=""
+                                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    >
+                                        </textarea>
+                                        <label
+                                        htmlFor="floating_direccion"
+                                        className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                        >
+                                        Direccion
+                                        </label>
+                                    </div>
+                                    <div className="relative z-0 w-full mb-5 group">
+                                        <select name="" id="servicios"
+                                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                        >
+                                        <option> </option>
+                                        
+                                        </select>
+                                        <label
+                                            htmlFor="servicios"
+                                            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                        >
+                                            Servicios
+                                        </label>
+                                </div>         
+                            </div>
+                            <button
+                                type="submit"
+                                className="w-full md:w-auto px-4 bg-gold py-2 my-1 rounded-full text-white"                              
+                            >
+                                Guardar
+                            </button>
+                        </form>
+                        <button className="w-full md:w-auto md:px-4 bg-black py-2 my-1 rounded-full text-white" onClick={cerrarModal}>Cerrar</button>         
+                    </div>
+                    
+                </StyledModal>
+            </ModalProvider>
     </div>
   )
+  function cerrarModal(){
+    
+    toggleModal()
+    setEmpleadoActual({})
+    console.log(empleadoActual)
+  }
 }
 
 export default Page
--- src/controllers/EmpleadosController.js
+++ src/controllers/EmpleadosController.js
@@ -4,3 +4,7 @@ export async function getEmpleados(){
     const empleados = await axios.get( `${process.env.NEXT_PUBLIC_API_URL}/empleados`);
     return empleados.data
 }
+export async function getEmpleadoById(id) {
+    const empleados = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/empleados/${id}`)
+    return empleados.data
+}
