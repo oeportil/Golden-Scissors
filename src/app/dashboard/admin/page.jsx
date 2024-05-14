f5a044ca7ad446932e45f4d96a4301db30464a71
JonathanAva <hpalvarez10@gmail.com>
2024-05-14 03:05:31 -0600

    modal pero se me chuecan los id

--- src/app/dashboard/admin/page.jsx
+++ src/app/dashboard/admin/page.jsx
@@ -11,6 +11,58 @@ const page = () => {
       <div className="flex justify-between">
         <div className="w-1/2 mr-4">
           <div className='justify-center text-center'>
+            <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+              Serch
+            </button>
+
+            <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+              <div class="relative p-4 w-full max-w-md max-h-full">
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                  <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                  </button>
+                  <div class="p-4 md:p-5 text-center">
+                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Product Details</h3>
+
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                          <th scope="col" class="px-6 py-3">ID</th>
+                          <th scope="col" class="px-6 py-3">Name</th>
+                          <th scope="col" class="px-6 py-3">Price</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                          <td class="px-6 py-4">1</td>
+                          <td class="px-6 py-4">Product A</td>
+                          <td class="px-6 py-4">$20.00</td>
+                        </tr>
+                        <tr class="bg-gray-50 dark:bg-gray-800">
+                          <td class="px-6 py-4">2</td>
+                          <td class="px-6 py-4">Product B</td>
+                          <td class="px-6 py-4">$35.00</td>
+                        </tr>
+                        <tr class="bg-white dark:bg-gray-800">
+                          <td class="px-6 py-4">3</td>
+                          <td class="px-6 py-4">Product C</td>
+                          <td class="px-6 py-4">$45.00</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div class="mt-4">
+                      <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                        Close
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
             <h2 className="texto  font-bold text-lg mb-4">Reservaciones</h2>
           </div>
 
@@ -76,8 +128,8 @@ const page = () => {
                   <div class="relative p-4 w-full max-w-md max-h-full">
                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                       <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
-                        
-                       
+
+
                         <span class="sr-only">Close modal</span>
                       </button>
                       <div class="p-4 md:p-5 text-center">
@@ -111,8 +163,8 @@ const page = () => {
                   <div class="relative p-4 w-full max-w-md max-h-full">
                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                       <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
-                        
-                       
+
+
                         <span class="sr-only">Close modal</span>
                       </button>
                       <div class="p-4 md:p-5 text-center">
