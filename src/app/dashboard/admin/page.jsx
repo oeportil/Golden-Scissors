ef1b110967e2c382542f72a79af773e43e483bc2
JonathanAva <hpalvarez10@gmail.com>
2024-05-15 02:15:21 -0600

    modal detalle cliente

--- src/app/dashboard/admin/page.jsx
+++ src/app/dashboard/admin/page.jsx
@@ -115,9 +115,87 @@ const page = () => {
                 <p>7234-2357</p>
                 <p>Corte tradicional, Barba</p>
                 <p>4:30 p.m.</p>
-                <button className="boton1 texto font-bold py-2 px-4 rounded">
+
+                <button data-modal-target="default-modal" data-modal-toggle="default-modal" class="boton1 texto font-bold py-2 px-4 rounded" type="button">
                   Detalles
                 </button>
+
+
+                <div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                  <div class="relative p-4 w-full max-w-2xl max-h-full">
+
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+
+                      <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                          DETALLES
+                        </h3>
+                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                          </svg>
+                          <span class="sr-only">Close modal</span>
+                        </button>
+                      </div>
+
+                      <div class="p-4 md:p-5 space-y-4">
+                        <div class="text-gray-900 dark:text-white">
+                          <div class="mb-4">
+                            <div class="font-bold">José Padilla</div>
+                            <div>7234-2357</div>
+                          </div>
+                          <div class="mb-4">
+                            <div class="flex justify-between">
+                              <span>Corte Tradicional</span>
+                              <span>$5</span>
+                            </div>
+                            <div class="flex justify-between">
+                              <span>Crew</span>
+                              <span>4:30 p.m. - 5:15 p.m.</span>
+                            </div>
+                            <div class="flex justify-between">
+                              <span>Kruma Lui</span>
+                            </div>
+                          </div>
+                          <div class="mb-4">
+                            <div class="flex justify-between">
+                              <span>Barba</span>
+                              <span>$4</span>
+                            </div>
+                            <div class="flex justify-between">
+                              <span>Candado</span>
+                              <span>5:15 p.m. - 5:30 p.m.</span>
+                            </div>
+                            <div class="flex justify-between">
+                              <span>Kruma Lui</span>
+                            </div>
+                          </div>
+                          <div class="mb-4">
+                            <div class="flex justify-between">
+                              <span>Delineado</span>
+                              <span>$3</span>
+                            </div>
+                            <div class="flex justify-between">
+                              <span>Cejas</span>
+                              <span>5:30 p.m. - 5:45 p.m.</span>
+                            </div>
+                            <div class="flex justify-between">
+                              <span>Carlos Guerra</span>
+                            </div>
+                          </div>
+                          <div class="font-bold">25/05/2024</div>
+                          <div>4:30 p.m. - 5:45 p.m.</div>
+                          <div class="font-bold text-right">$12</div>
+                        </div>
+                      </div>
+
+                      <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                        <button data-modal-hide="default-modal" type="button" class="text-white boton1 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cerrar</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
                 <button className="boton2 texto font-bold py-2 px-4 rounded ml-2">
                   Cancelar
                 </button>
@@ -189,7 +267,7 @@ const page = () => {
             <h2 className="texto font-bold text-lg mb-4">Peluqueros actuales</h2>
           </div>
           <div className="p-1 boton1 mb-7">
-            <div className="reser text-white p-4 py-4 flex items-center justify-between rounded-lg" style={{height:'19vh'}}>
+            <div className="reser text-white p-4 py-4 flex items-center justify-between rounded-lg" style={{ height: '19vh' }}>
               <div>
                 <h3 className="font-bold">Walter white</h3>
                 <p>Corte tradicional, Barba, Delineado, Tintado, Ajuste.</p>
@@ -232,7 +310,7 @@ const page = () => {
                           />
                         </svg>
                         <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                        Walter white
+                          Walter white
                         </h3>
                         <button
                           data-modal-hide="popup-modal3"
@@ -254,12 +332,12 @@ const page = () => {
                 </div>
               </div>
               <div className="flex items-center">
-         
+
               </div>
             </div>
           </div>
           <div className="p-1 boton1 mb-4">
-            <div className="reser text-white p-4 py-4 flex items-center justify-between rounded-lg" style={{height:'19vh'}}>
+            <div className="reser text-white p-4 py-4 flex items-center justify-between rounded-lg" style={{ height: '19vh' }}>
               <div>
                 <h3 className="font-bold">Carlos Guerra</h3>
                 <p>Corte tradicional, Barba, Delineado, Tintado, Ajuste.</p>
@@ -302,7 +380,7 @@ const page = () => {
                           />
                         </svg>
                         <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                        Carlos Guerra
+                          Carlos Guerra
                         </h3>
                         <button
                           data-modal-hide="popup-modal"
@@ -324,20 +402,20 @@ const page = () => {
                 </div>
               </div>
               <div className="flex items-center">
-   
+
               </div>
             </div>
           </div>
 
           <div className="p-1 mt-6" >
-            <div className="reser text-white p-4 py-4 flex items-center justify-between rounded-lg" style={{height:'19vh'}}>
+            <div className="reser text-white p-4 py-4 flex items-center justify-between rounded-lg" style={{ height: '19vh' }}>
               <div>
                 <h3 className="font-bold">Carlos Guerra</h3>
                 <p>Corte tradicional, Barba, Delineado, Tintado, Ajuste.</p>
                 <p className="text-white">Atendiendo - #5624561</p>
               </div>
               <div className="flex items-center">
-               
+
               </div>
             </div>
           </div>
