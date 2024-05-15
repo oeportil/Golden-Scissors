b75dd05be0155efcd4ce460bd96d4538ee6efc7b
JonathanAva <hpalvarez10@gmail.com>
2024-05-14 22:17:31 -0600

    fix

--- src/app/dashboard/admin/page.jsx
+++ src/app/dashboard/admin/page.jsx
@@ -8,9 +8,9 @@ const page = () => {
   return (
     <div className="p-8 fondo text-white min-h-screen">
        <button
-              data-modal-target="popup-modal"
+              data-modal-target="popup-modal1"
               data-modal-toggle="popup-modal"
-              class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="block text-white boton1  focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
               type="button"
             >
               Search
@@ -21,7 +21,7 @@ const page = () => {
           <div className="justify-center text-center">
            
             <div
-              id="popup-modal"
+              id="popup-modal1"
               tabindex="-1"
               class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
             >
